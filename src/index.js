a443939806d5d3528b25b5992ec6979dfeada9e9
Manuel Herrera <manuel.herrera@owasp.org>
2023-01-13 09:27:34 -0300

    fix peer dependencies, update apollo, react and more

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Data from './Data'
-import {graphql} from 'react-apollo'
+import {graphql} from '@apollo/client/react/hoc'
 import gql from 'graphql-tag'
 import getQueryFields from './getQueryFields'
 import {getQueryParams, getVariables} from './getParams'
